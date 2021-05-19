77bac0408fb02bee3b60505aef1a6c83de831904
anil sidhu <webanilsidhu@gmail.com>
2021-05-19 16:36:01 +0530

    done with default

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,12 +2,10 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { UserListComponent } from './user-list/user-list.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    UserListComponent,
   ],
   imports: [
     BrowserModule
