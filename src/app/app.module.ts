7dc2ee8a86ce791bd9bfa8520ecba71a6a5d3ff5
anil sidhu <webanilsidhu@gmail.com>
2021-05-19 16:34:27 +0530

    done with component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { UserListComponent } from './user-list/user-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UserListComponent,
   ],
   imports: [
     BrowserModule
