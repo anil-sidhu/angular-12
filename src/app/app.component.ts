e997bcabad9536337a5e609b00bf16ad329f9b1b
anil sidhu <webanilsidhu@gmail.com>
2021-05-22 21:57:47 +0530

    done with text box value

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,10 +3,14 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'blog';
-  data="hello code "
-  num=100
+  title = 'Get Input Box Value';
+  displayVal='';
+  getValue(val:string)
+  {
+    console.warn(val)
+    this.displayVal=val
+  }
 }
