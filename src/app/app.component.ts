75557a8002b2b742cebcedbecddb650413058af1
anil sidhu <webanilsidhu@gmail.com>
2021-06-06 21:31:33 +0530

    done with if else

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,7 +6,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Property Binding';
-  name="Peter";
-  disable=false
+  title = 'If-else Condition';
+  show=true
 }
