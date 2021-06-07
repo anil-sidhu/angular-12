612fc500f9f93b98c81a49626d4deb1b947204e2
anil sidhu <webanilsidhu@gmail.com>
2021-06-07 21:46:25 +0530

    done with else if

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,6 +6,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'If-else Condition';
-  show=true
+  title = 'Multiple condition or else if';
+  color="blue";
 }
