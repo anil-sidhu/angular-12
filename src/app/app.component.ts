b91869697a50c88b9ef09bd8448e348f6c9e7f6e
anil sidhu <webanilsidhu@gmail.com>
2021-06-08 21:23:41 +0530

    done with switch case

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,6 +6,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Multiple condition or else if';
-  color="blue";
+  title = 'Switch Case';
+  color="redd";
 }
