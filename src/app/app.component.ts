660c1484b8d735d35e876ce10726db48484765a5
anil sidhu <webanilsidhu@gmail.com>
2021-06-10 21:11:46 +0530

    done with loop

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,9 +3,15 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'Switch Case';
-  color="redd";
+  title = 'Nested For Loop';
+  users=[
+    {name:'Anil',phone:'1111',email:'anil@test.com',accounts:['facebook','gmail','linkedin']},
+    {name:'Sam',phone:'2222',email:'sam@test.com',accounts:['Youtube','yahoo','linkedin']},
+    {name:'Peter',phone:'3333',email:'peter@test.com',accounts:['Yahoo','insta','linkedin']},
+    {name:'Bruce',phone:'2299',email:'bruce@test.com',accounts:['Gmail','gmail','youtube']},
+
+  ]
 }
