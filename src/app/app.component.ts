2e2a5e3cd55c28ed7caff96267f5dbe6c84dad22
anil sidhu <webanilsidhu@gmail.com>
2021-06-21 21:37:25 +0530

    done with child to parent

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,7 +6,10 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Send data child to parent Component';
+  data="x";
+  updateData(item:string)
+  {
+    console.warn(item)
+    this.data=item;
+  }
 }
-
-
-
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,9 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
+import { ChildComponent } from './child/child.component';
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, ChildComponent],
   imports: [BrowserModule],
   providers: [],
   bootstrap: [AppComponent],
